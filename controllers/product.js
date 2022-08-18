9192c4c952b1710b787afd2f3fbe917c9fd67ff3
Ngo Minh Nhut <minhnhut161098@gmail.com>
2022-08-18 14:19:56 +0700

    ok

--- controllers/product.js
+++ controllers/product.js
@@ -6,7 +6,7 @@ async function beforeActions() {
     const createProductsQuery = `create table if not exists products(
         id varchar(200) NOT NULL,
         name varchar(255) not null,
-        created_at datetime not null default CURRENT_TIMESTAMP
+        type varchar(255) not null
     ) ENGINE=InnoDB DEFAULT CHARSET=latin1;`;
 
     await connection.execute(createProductsQuery)
